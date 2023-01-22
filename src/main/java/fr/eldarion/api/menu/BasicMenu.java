d86430aea960afd72034a8581942bc4247e518a3
LeGarri <riobusor66@gmail.com>
2023-01-22 12:04:33 +0100

    added click event and constructor

--- src/main/java/fr/eldarion/api/menu/BasicMenu.java
+++ src/main/java/fr/eldarion/api/menu/BasicMenu.java
@@ -1,13 +1,37 @@
 package fr.eldarion.api.menu;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.plugin.Plugin;
 
-public class BasicMenu {
+public class BasicMenu implements Listener {
 
-	public void createMenu() {
+	public String title;
+	
+	private Inventory inventory;
+	
+	
+	
+	public BasicMenu(Plugin instance, String title, InventorySize size) {
+		this.title = title;
+		createInventory(size, title);
 		
+		Bukkit.getPluginManager().registerEvents(this, instance);
 	}
+	
+	
+	
+	public Inventory getInventory() {
+		return this.inventory;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	
 	
 	private Inventory createInventory(InventorySize size, String name) {
 		Inventory inv = Bukkit.createInventory(null, size.value(), name);
@@ -15,4 +39,30 @@ public class BasicMenu {
 		return inv;
 	}
 	
+	
+	
+	public void onPlayerInteractInventory(InventoryClickEvent e) {
+		if(e.getView().getTitle() == this.getTitle()) {
+			e.setCancelled(true);
+			e.getWhoClicked().sendMessage("test");
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
