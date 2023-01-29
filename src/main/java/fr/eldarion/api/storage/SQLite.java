f944b31cf61f60a73b33f88ebc2d84f743bb2642
LeGarri <riobusor66@gmail.com>
2023-01-29 17:53:42 +0100

    added query method

--- src/main/java/fr/eldarion/api/storage/SQLite.java
+++ src/main/java/fr/eldarion/api/storage/SQLite.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
@@ -37,17 +39,20 @@ public class SQLite {
         }
         
         try {
-            if(connection!=null&&!connection.isClosed()){
+            if(connection != null && !connection.isClosed()) {
                 return connection;
             }
+            
             Class.forName("org.sqlite.JDBC");
             connection = DriverManager.getConnection("jdbc:sqlite:" + dataFolder);
+            
             return connection;
         } catch (SQLException ex) {
             instance.getLogger().log(Level.SEVERE, "SQLite exception on initialize", ex);
         } catch (ClassNotFoundException ex) {
             instance.getLogger().log(Level.SEVERE, "You need the SQLite JBDC library. Google it. Put it in /lib folder.");
         }
+        
         return null;
 	}
 	
@@ -61,14 +66,36 @@ public class SQLite {
 		
 		query += ");";
 		
-		executeQuery(query);
+		executeNotSafeUpdateQuery(query);
 	}
 	
-	public void executeQuery(String query) {
+	private void executeNotSafeUpdateQuery(String query) {
 		try {
             Statement s = connection.createStatement();
             s.executeUpdate(query);
             s.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public ResultSet executeQuery(PreparedStatement statement) {
+		ResultSet rs = null;
+		
+		try {
+            rs = statement.executeQuery();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+		
+		return rs;
+	}
+	
+	public void executeUpdateQuery(PreparedStatement statement) {
+		try {
+            statement.executeUpdate();
+            statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
